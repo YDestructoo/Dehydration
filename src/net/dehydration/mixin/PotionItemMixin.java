1a1ddc9139cd6c6cddf17ba0e4dcea863a25b2c8
Matheo Jay Marquez <150364132+YDestructoo@users.noreply.github.com>
2026-08-07 23:38:14 +0800

    fix: remove obsolete PotionItem use injection

--- src/net/dehydration/mixin/PotionItemMixin.java
+++ src/net/dehydration/mixin/PotionItemMixin.java
@@ -6,50 +6,25 @@ import net.dehydration.misc.ThirstTooltipData;
 import net.dehydration.util.ThirstHelper;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.PotionContentsComponent;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PotionItem;
+import net.minecraft.item.ThrowablePotionItem;
 import net.minecraft.item.tooltip.TooltipData;
-import net.minecraft.registry.tag.FluidTags;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Hand;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.RaycastContext;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
 @Mixin(PotionItem.class)
 public abstract class PotionItemMixin extends Item {
 
-    public PotionItemMixin(net.minecraft.item.Item.Settings settings) {
+    public PotionItemMixin(Item.Settings settings) {
         super(settings);
     }
-
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    private void useMixin(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<ActionResult> info) {
-        BlockHitResult hitResult = Item.raycast(world, user, RaycastContext.FluidHandling.SOURCE_ONLY);
-        if (((HitResult) hitResult).getType() == HitResult.Type.BLOCK) {
-            BlockPos blockPos = hitResult.getBlockPos();
-            if (world.canEntityModifyAt(user, blockPos) && world.getFluidState(blockPos).isIn(FluidTags.WATER)) {
-                world.playSound(user, user.getX(), user.getY(), user.getZ(), SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.NEUTRAL, 1.0f, 1.0f);
-                info.setReturnValue((world.isClient() ? ActionResult.SUCCESS : ActionResult.SUCCESS_SERVER).withNewHandStack(new ItemStack(Items.GLASS_BOTTLE)));
-            }
-        }
-    }
-
 
     @Override
     public Optional<TooltipData> getTooltipData(ItemStack stack) {
         if (ConfigInit.CONFIG.thirst_preview && !(stack.getItem() instanceof ThrowablePotionItem)) {
-
             int thirstQuench = 0;
             for (int i = 0; i < DehydrationMain.HYDRATION_TEMPLATES.size(); i++) {
                 if (DehydrationMain.HYDRATION_TEMPLATES.get(i).containsItem(stack.getItem())) {
@@ -60,12 +35,12 @@ public abstract class PotionItemMixin extends Item {
             if (thirstQuench == 0) {
                 thirstQuench = ConfigInit.CONFIG.potion_thirst_quench;
             }
-            if (stack.getOrDefault(DataComponentTypes.POTION_CONTENTS, PotionContentsComponent.DEFAULT).potion().isPresent() && ThirstHelper.isBadPotion(stack.getOrDefault(DataComponentTypes.POTION_CONTENTS, PotionContentsComponent.DEFAULT).potion().get())) {
+            if (stack.getOrDefault(DataComponentTypes.POTION_CONTENTS, PotionContentsComponent.DEFAULT).potion().isPresent()
+                    && ThirstHelper.isBadPotion(stack.getOrDefault(DataComponentTypes.POTION_CONTENTS, PotionContentsComponent.DEFAULT).potion().get())) {
                 return Optional.of(new ThirstTooltipData(2, thirstQuench));
             }
             return Optional.of(new ThirstTooltipData(0, thirstQuench));
         }
         return Optional.empty();
     }
-
 }
