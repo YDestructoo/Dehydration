43edc66d747c132e35429728208bad951c6b0574
Matheo Jay Marquez <150364132+YDestructoo@users.noreply.github.com>
2026-08-07 23:27:58 +0800

    fix: correct 1.21.11 render registration symbols

--- src/net/dehydration/init/RenderInit.java
+++ src/net/dehydration/init/RenderInit.java
@@ -7,22 +7,25 @@ import net.dehydration.thirst.ThirstHudRender;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.TooltipComponentCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererFactories;
 import net.minecraft.client.render.BlockRenderLayer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 import net.minecraft.util.Identifier;
 
 public class RenderInit {
     public static final Identifier THIRST_ICON = Identifier.of("dehydration", "textures/gui/thirst.png");
 
     public static void init() {
-        BlockRenderLayerMap.putBlock(BlockInit.RAINWATER_COLLECTOR_BLOCK, BlockRenderLayer.CUTOUT);
-        BlockRenderLayerMap.putBlock(BlockInit.RAINWATER_LEVELED_COLLECTOR_BLOCK, BlockRenderLayer.CUTOUT);
-        BlockRenderLayerMap.putBlock(BlockInit.BAMBOO_PUMP_BLOCK, BlockRenderLayer.CUTOUT);
         BlockRenderLayerMap.putBlock(BlockInit.CAMPFIRE_CAULDRON_BLOCK, BlockRenderLayer.CUTOUT);
         BlockRenderLayerMap.putBlock(BlockInit.COPPER_CAULDRON_BLOCK, BlockRenderLayer.CUTOUT);
         BlockRenderLayerMap.putBlock(BlockInit.COPPER_WATER_CAULDRON_BLOCK, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(BlockInit.COPPER_POWDERED_CAULDRON_BLOCK, BlockRenderLayer.CUTOUT);
         BlockRenderLayerMap.putBlock(BlockInit.COPPER_PURIFIED_WATER_CAULDRON_BLOCK, BlockRenderLayer.CUTOUT);
-        BlockRenderLayerMap.putFluids(BlockRenderLayer.TRANSLUCENT, FluidInit.PURIFIED_WATER, FluidInit.FLOWING_PURIFIED_WATER);
+        BlockRenderLayerMap.putBlock(BlockInit.BAMBOO_PUMP_BLOCK, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(BlockInit.RAINWATER_COLLECTOR_BLOCK, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(BlockInit.RAINWATER_WATER_COLLECTOR_BLOCK, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(BlockInit.RAINWATER_POWDERED_COLLECTOR_BLOCK, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(BlockInit.RAINWATER_PURIFIED_WATER_COLLECTOR_BLOCK, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putFluids(BlockRenderLayer.TRANSLUCENT, FluidInit.PURIFIED_WATER, FluidInit.PURIFIED_FLOWING_WATER);
 
         BlockEntityRendererFactories.register(BlockInit.BAMBOO_PUMP_ENTITY, BambooPumpRenderer::new);
         TooltipComponentCallback.EVENT.register(data -> data instanceof ThirstTooltipData thirstData ? new ThirstTooltipComponent(thirstData) : null);
